9d5378256a8409350fdc7cdd85bdc68c13fa3670
Nicole Dow <nikki.dow71@gmail.com>
2019-04-09 21:38:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
+  if int % 3 == 0 
+    "Fizz"
+  end
   
+  if int % 5 == 0
+    "Buzz"
+  end
+  
+  if (int % 3 == 0 && int % 5 == 0)
+    "Fizzbuzz"
+  end
 end
